a1f5078ee25db43560b5bf7221e6a0c4ff3be53f
SnipUndercover <snipundercover@gmail.com>
2022-05-22 18:40:39 +0200

    Make `install.sh` fail on any error

--- install.sh
+++ install.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-
-function fail() {
-  echo "$(tput setaf 1)Aborting.$(tput sgr0)"
-  exit "$1"
-}
+set -e
 
 SCRIPT_PATH="${0%/*}"
-cd "$SCRIPT_PATH" || fail 1
+cd "$SCRIPT_PATH"
 SCRIPT_PATH="$PWD"
 
 echo "$(tput setaf 3)"Installing dependencies..."$(tput sgr0)"
 
 if ! [ -x "$(command -v sudo)" ]; then
   echo 'Could not find sudo. Please install sudo to use this script.'
-  fail 2
+  exit 2
 fi
-if ! sudo apt install -y curl git; then
-  echo 'Could not install dependencies.'
-  fail 2
-fi
+
+sudo apt install -y -qq curl git
 
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Creating .dotfile symlinks..."$(tput sgr0)"
@@ -28,17 +22,12 @@ ln -sf "$SCRIPT_PATH"/.gitconfig ~/.gitconfig
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Installing bat and exa..."$(tput sgr0)"
 
-if ! sudo apt install -y bat exa; then
-  echo "$(tput setaf 3)"An error occurred while installing bat and exa, continuing."$(tput sgr0)"
-fi
+sudo apt install -y -qq bat exa
 
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Configuring ZSH and oh-my-zsh..."$(tput sgr0)"
 
-if ! sudo apt install zsh -y; then
-  echo "$(tput setaf 3)"An error occurred while installing zsh. Cannot proceed further."$(tput sgr0)"
-  fail 2;
-fi
+sudo apt install -y -qq zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 ln -sf "$SCRIPT_PATH"/.zshrc ~/.zshrc
 chsh -s "$(which zsh)"
@@ -46,36 +35,29 @@ chsh -s "$(which zsh)"
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Configuring nvm..."$(tput sgr0)"
 
-cd ~ || fail 1
+cd ~
 git clone https://github.com/nvm-sh/nvm.git
-cd nvm || fail 1
+cd nvm
 git checkout "$(git tag -l | sort -Vr | head -1)"
 # shellcheck disable=SC1091
 . ./nvm.sh
 nvm install node
 npm install -g typescript
-cd "$SCRIPT_PATH" || fail 1
+cd "$SCRIPT_PATH"
 
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Configuring thefuck..."$(tput sgr0)"
 
-if ! sudo apt install -y python3 python3-pip python3-dev python3-setuptools; then 
-  echo "$(tput setaf 3)"An error occurred while installing python. Cannot proceed further."$(tput sgr0)"
-  fail 2;
-fi
-if ! pip3 install thefuck --user; then
-  echo "$(tput setaf 3)"An error occurred while installing thefuck, continuing."$(tput sgr0)"
-fi
+sudo apt install -y python3 python3-pip python3-dev python3-setuptools
+pip3 install thefuck --user
 
 echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Installing Java 8 and 18..."$(tput sgr0)"
 
-if ! sudo apt install -y \
+sudo apt install -y -qq \
   openjdk-18-jdk-headless \
   openjdk-18-jre-headless \
   openjdk-8-jdk-headless \
-  openjdk-8-jre-headless; then
-  echo "$(tput setaf 3)"An error occurred while installing Java 18 and 8, continuing."$(tput sgr0)"
-fi
+  openjdk-8-jre-headless
 
 echo "$(tput setaf 2)"...done. Exit and reopen the terminal to see effects."$(tput sgr0)"
