141fa5015addbf3be6fda54699e737c0cf80210b
SnipUndercover <snipundercover@gmail.com>
2022-05-22 20:02:10 +0200

    Inform user about `chsh -s` command

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ echo "$(tput setaf 2)"...done"$(tput sgr0)"
 echo "$(tput setaf 3)"Configuring ZSH and oh-my-zsh..."$(tput sgr0)"
 
 sudo apt install -y -qq zsh
+echo "$(tput setaf 3)"Your default shell is about to be changed into "$(which zsh)"."$(tput sgr0)"
+echo "$(tput setaf 3)"You will be prompted for your password to confirm."$(tput sgr0)"
 chsh -s "$(which zsh)"
 ln -sf "$SCRIPT_PATH"/.zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
